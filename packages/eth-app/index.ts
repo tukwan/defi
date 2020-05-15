3080ed7f4e2b3ac1ad8baef53e719d0289473414
zbiegien <zbiegien@protonmail.com>
2020-05-15 19:18:59 +0100

    Node ABIS exporter

---
+++ packages/eth-app/index.ts
@@ -0,0 +1,13 @@
+import Counter from './abis/Counter.json'
+import ERC20Detailed from './abis/ERC20Detailed.json'
+import IERC20 from './abis/IERC20.json'
+import Migrations from './abis/Migrations.json'
+import ZBToken from './abis/ZBToken.json'
+
+export const ABIS = {
+  Counter,
+  ERC20Detailed,
+  IERC20,
+  Migrations,
+  ZBToken
+}
---
+++ packages/eth-app/utils/abis_exporter.ts
@@ -0,0 +1,35 @@
+const fs = require('fs')
+const path = require('path')
+
+// Consts
+const indexFilePath = path.resolve('./', 'index.ts')
+const abisPath = path.resolve('./', 'abis')
+const abisFiles = fs.readdirSync(abisPath)
+
+// Builders
+const safeVariableName = fileName => {
+  const indexOfDot = fileName.indexOf('.')
+  return fileName.slice(0, indexOfDot)
+}
+
+const buildImportBlocks = files => {
+  const importBlocks = files.map(
+    fileName => 'import ' + safeVariableName(fileName) + " from './abis/" + fileName + "'"
+  )
+  const importBlocksJoined = importBlocks.join('\n')
+  return importBlocksJoined
+}
+
+const buildExportBlocks = files => {
+  const startExport = 'export const ABIS = {\n'
+  const endExport = '\n}'
+  const exportBlocks = files.map(fileName => '  ' + safeVariableName(fileName))
+  const exportBlocksJoined = exportBlocks.join(',\n')
+  const finalExport = startExport + exportBlocksJoined + endExport
+  return finalExport
+}
+
+const indexCode = files => buildImportBlocks(files) + '\n\n' + buildExportBlocks(files)
+
+// Writer
+fs.writeFileSync(indexFilePath, indexCode(abisFiles))
