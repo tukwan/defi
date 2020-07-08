345d549432e768fba015dd9cafb10fa790bbf06b
deployAt <mikowhy@tutanota.com>
2020-07-08 21:39:29 +0100

    feat: add money legos & synthetix

--- packages/eth-app/truffle-config.js
+++ packages/eth-app/truffle-config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const HDWalletProvider = require('@truffle/hdwallet-provider')
 
-const { INFURA_PROJECT_ID, MNEMONIC, PRIVATE_KEY_RINKEBY } = process.env
+const { INFURA_PROJECT_ID, MNEMONIC, PRIVATE_KEY_RINKEBY, PRIVATE_KEY_KOVAN } = process.env
 
 module.exports = {
   networks: {
@@ -23,19 +23,19 @@ module.exports = {
       gasPrice: 10000000000,
       skipDryRun: true
     },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(PRIVATE_KEY_KOVAN, 'https://kovan.infura.io/v3/' + INFURA_PROJECT_ID)
+      },
+      network_id: '42',
+      gas: 4465030,
+      gasPrice: 10000000000,
+    },
     ropsten: {
       provider: function() {
         return new HDWalletProvider(MNEMONIC, 'https://ropsten.infura.io/v3/' + INFURA_PROJECT_ID)
       },
       network_id: '3',
-      gas: 4465030,
-      gasPrice: 10000000000
-    },
-    kovan: {
-      provider: function() {
-        return new HDWalletProvider(MNEMONIC, 'https://kovan.infura.io/v3/' + INFURA_PROJECT_ID)
-      },
-      network_id: '42',
       gas: 4465030,
       gasPrice: 10000000000
     },
